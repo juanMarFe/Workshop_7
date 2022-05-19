1fa40f97a25ff1479dafff26ec8ee65020afcdd1
juanMarFe <71792805+juanMarFe@users.noreply.github.com>
2022-05-19 00:05:59 -0500

    Update Slave.ino

--- Slave.ino
+++ Slave.ino
@@ -1,38 +1,37 @@
+//Importe de la libreria de I2C
 #include "Wire.h"
 
+// Identificador de esclavo
 const byte I2C_SLAVE_ADDR = 0x20;
 
 void setup()
 {
    Serial.begin(115200);
+   // Inicialización del pin en el que se encuentra el sensor
    pinMode(A0,INPUT);
+   // Inicialización del metodo identificador de esclavo
    Wire.begin(I2C_SLAVE_ADDR);
-   Wire.onReceive(receiveEvent);
+   // Indicador de metodo a utilizar en caso de que el maestro lo necesite
    Wire.onRequest(requestEvent);
 }
 
-long data = 0;
-//long response = 200;
-
-
-void receiveEvent(int bytes)
-{
-   data = 0;
-   uint8_t index = 0;
-   while (Wire.available())
-   {
-      byte* pointer = (byte*)&data;
-      *(pointer + index) = (byte)Wire.read();
-      index++;
-   }
-}
-
+//Metodo que lee el sensor en el momento que el maestro lo indica
 void requestEvent()
 {
+   //Lectura del Sensor
   float i = (float)analogRead(A0);
+   
+////////////////////////////////////////////////////////////////////////////////
+   
+  //Cambio de los bytes dados por el sensor a la temperatura en celcius
+  
   float y = (i*5000/1023)/10;
   Serial.println((int)y);
   long p=(long)y;
+   
+////////////////////////////////////////////////////////////////////////////////
+   
+  //Envio de información al Maestro
   Wire.write((byte*)&p, sizeof(p));
 }
 
